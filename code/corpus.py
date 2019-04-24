7c8a41c098942b8b9bd595de5ea1fcaf6116f91d
Jeffery Zhang <jzhang1@gmail.com>
2019-04-23 22:38:09 -0400

    update

--- code/corpus.py
+++ code/corpus.py
@@ -1,16 +1,21 @@
 import os
 import spacy
+import tqdm
 
 class Corpus:
-    def __init__(self, dir_path):
+    def __init__(self, dir_path, show_progress = True):
         """
         Creates a corpus from all files in a directory
         """
         self.dir_path = os.path.abspath(dir_path)
+        self.show_progress = show_progress
 
     def __iter__(self):
         en = spacy.load('en')
-        for file in os.listdir(self.dir_path):
+        dir_list = os.listdir(self.dir_path)
+        if self.show_progress:
+            dir_list = tqdm.tqdm(dir_list)
+        for file in dir_list:
             filepath = os.path.join(self.dir_path, file)
             with open(filepath, mode = 'r', encoding = 'utf-8') as f:
                 for index, sentence in enumerate(en(f.read()).sents):
