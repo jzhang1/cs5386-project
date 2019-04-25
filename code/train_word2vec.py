484b0480e3a97d80fb8e473af9eb22653586158f
Jeffery Zhang <jzhang1@gmail.com>
2019-04-25 16:26:19 -0400

    update

--- code/train_word2vec.py
+++ code/train_word2vec.py
@@ -9,11 +9,12 @@ import numpy as np
 import os
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Embedding, Lambda, Dense, Input
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras import backend as K
 from from_tfrecord import load_dataset
 from create_tokenizer import load_tokenizer
 
-# python code\train_word2vec.py --tokenizer_file "tokenizer.pickle" --dataset_dir "tfrecord" --output_file "word2vec.h5"
+# python code\train_word2vec.py --tokenizer_file "tokenizer.pickle" --dataset_dir "tfrecord" --output_file "word2vec.hdf5" --checkpoint_dir "checkpoints"
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -21,6 +22,7 @@ def parse_args():
     parser.add_argument("--dataset_dir", dest="dataset_dir", type=str, required=True)
     parser.add_argument("--num_parallel_calls", dest="num_parallel_calls", type=int, required=False)
     parser.add_argument("--output_file", dest="output_file", type=str, required=True)
+    parser.add_argument("--checkpoint_dir", dest="checkpoint_dir", type=str, required=True)
     parser.add_argument("--train_batch_size", dest="train_batch_size", type=int, default=32, required=False)
     parser.add_argument("--train_steps_per_epoch", dest="train_steps_per_epoch", type=int, default=128, required=False)
     parser.add_argument("--train_epochs", dest="train_epochs", type=int, default=20, required=False)
@@ -54,5 +56,11 @@ if __name__ == "__main__":
 
     dataset = load_dataset(args.dataset_dir)
 
-    model.fit(dataset.repeat().batch(args.train_batch_size).make_one_shot_iterator(), steps_per_epoch = args.train_steps_per_epoch, epochs = args.train_epochs)
+    checkpoint_path = os.path.join(args.checkpoint_dir, "word2vec.{epoch:02d}-{loss:.2f}.hdf5")
+    callbacks_list = [
+        EarlyStopping(monitor='loss', min_delta=0.01, patience=10, mode='min', verbose=1, restore_best_weights=True),
+        ModelCheckpoint(checkpoint_path, monitor='loss', verbose=1, save_best_only=True, mode='min', save_weights_only=True)
+    ]
+
+    model.fit(dataset.repeat().batch(args.train_batch_size).make_one_shot_iterator(), steps_per_epoch = args.train_steps_per_epoch, epochs = args.train_epochs, callbacks = callbacks_list)
     model.save(args.output_file)
